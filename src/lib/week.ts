5ddb2c545046c08290dec402030af0a6ec2f137d
basicbasja-cloud <basic.bas.ja@gmail.com>
2026-04-22 10:47:56 +0700

    Updates

--- src/lib/week.ts
+++
@@ -1,9 +0,0 @@
-import { addDays, getDay, startOfDay } from 'date-fns';
-
-export function getUpcomingSaturday(date: Date) {
-  const normalizedDate = startOfDay(date);
-  const dayOfWeek = getDay(normalizedDate);
-  const daysUntilSaturday = (6 - dayOfWeek + 7) % 7;
-
-  return addDays(normalizedDate, daysUntilSaturday);
-}
